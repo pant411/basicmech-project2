543a34d70dc5110eea4e48f1b671dc93a1be01be
pant411 <pantchanit2561@hotmail.com>
2021-11-08 22:41:42 +0700

    first

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1636386077,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ตึกpng-sheet0.png",
+		"images/images-sheet0.png",
+		"images/ดาวน์โหลด-sheet0.png",
+		"images/662873_pngfilesizesniperpng-sheet0.png",
+		"images/images2-sheet0.png",
+		"images/pngwingcom-sheet0.png",
+		"images/palaceroomroyalemptyinteriorempirestyle_-sheet0.png",
+		"images/รูปภาพ-sheet0.png",
+		"images/รูปภาพ2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bull-sheet0.png",
+		"images/ppppp-sheet0.png",
+		"media/gunsnglshotfireex-pe1107203.ogg",
+		"media/mouse-click-00-m-fesliyanstudios.com2.ogg",
+		"media/strategy (dramatic music) - vanoss gaming background music (hd).ogg",
+		"media/bullet-hitting-ground-sound-effect-_1_.ogg",
+		"media/applause-sound-effect2.ogg",
+		"media/fail-sound-effect.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
